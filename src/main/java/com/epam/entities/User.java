ab66e04b624f33e3bfb3438bd8f3eef54d97aa34
Blatman Rostislav <blatmanrostislav@mail.ru>
2018-07-02 12:28:45 +0300

    added lombok annotation @Value

--- src/main/java/com/epam/entities/User.java
+++ src/main/java/com/epam/entities/User.java
@@ -1,5 +1,8 @@
 package com.epam.entities;
 
+import lombok.Value;
+
+@Value
 public class User {
     public static User PITER_CHAILOVSKI = new User("epam", "1234",
             "PITER CHAILOVSKI");
@@ -7,23 +10,6 @@ public class User {
     private String password;
     private String userName;
 
-    private User(String login, String password, String userName) {
-        this.login = login;
-        this.password = password;
-        this.userName = userName;
-    }
-
-    public String getLogin() {
-        return login;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public String getUserName() {
-        return userName;
-    }
 
 
 }
